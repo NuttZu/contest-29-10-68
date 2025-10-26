d868e5da4df701feaa099d04955fcb8ccbf7c447
NuttZu <NuttZu.git@hotmail.com>
2025-10-26 19:06:17 +0700

    update WIRING.txt

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,6 @@
 #include <connectMqttBroker.h>
 
+                        //  wifi ssid         wifi pass     broker ip   broker port
 connectMqttBroker myMQTT("NuttZuRaspiWifi", "NuttZuWifi1", "10.42.0.1", 1883);
 
 void setup() {
